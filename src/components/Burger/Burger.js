e3662b644f6a6ded0999d9cd5b035da4a1da7bdd
Tushar <tushardahiya84@gmail.com>
2020-05-11 01:01:26 +0530

    displayed the burger ingredients

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import classes from '../Burger/Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+
+const transformedIngredients = Object.keys(props.ingredients)
+        .map( igKey => {
+            return [...Array(props.ingredients[igKey])].map((_,i)=> {
+               return <BurgerIngredient key={igKey + i } type={igKey} />
+            })
+        } );
+    
+        return(
+        <div className={classes.Burger}>
+            <BurgerIngredient type='Bread-Top' />
+            {transformedIngredients}
+            <BurgerIngredient type='Bread-Bottom' />
+        </div>
+    ); 
+};
+
+export default burger;
+
+
+      // LOGIC OF CONST TRASFORMEDINGREDIENTS
+    //   array of keys [meat,bacon....]
+
+        // map received meat for example
+        
+        // returns an array [    with length=(depends on the ingredient value)  ]
+        
+        
+        // this final array will have different length depending upon the value
+        // for example for meat the array is [ _,_ ] maybe 
+        
+        // on this array  (dont care about the value of the elements but their index 
+        // so map(_,i) => {<burgeringredient /> }
+        // this will run on each element of array with the key increasing each time according to the index
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
 import Auxiliary from '../../hoc/Auxiliary'; 
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients:{
+            Salad: 1,
+            Bacon:1,
+            Cheese:2,
+            Meat:2
+        }
+    };
+
     render(){
         return(
             <Auxiliary>
-                <div>Burger</div>
+                <Burger ingredients={this.state.ingredients} />
                 <div>Burger Controls</div>
             </Auxiliary>
         );
