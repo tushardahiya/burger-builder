966272d6d3fce78d40445672ed924047694a7588
Tushar <tushardahiya84@gmail.com>
2020-05-20 16:09:44 +0530

    added toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from '../Layout/Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout =(props) => (
     <Auxiliary>
-        <div>Toolbar,Sidebar,Backdrop</div>
+        <Toolbar/>
         <main className={classes.Content} > 
             {props.children}
         </main>
