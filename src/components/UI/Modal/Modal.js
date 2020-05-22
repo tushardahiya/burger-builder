6a3b5d0dbe9d204be9459fa2b38bdfacfbcb7bb0
Tushar <tushardahiya84@gmail.com>
2020-05-22 13:46:50 +0530

    changed folder structure

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import classes from './Modal.css';
 import Backdrop from '../Backdrop/Backdrop';
-import Auxiliary from '../../../hoc/Auxiliary';
+import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
 
 class Modal extends Component {
 
