52cdb3af8c786480ab2d418ff3e0a94f8f929db4
Tushar <tushardahiya84@gmail.com>
2020-06-01 20:59:12 +0530

    added navlinks from react-router-dom

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import classes from './NavigationItems.css';
-import NavigationItem from './NavigationItem/NavigationItem';
+import React from "react";
+import classes from "./NavigationItems.css";
+import NavigationItem from "./NavigationItem/NavigationItem";
 
-const navigationItems=()=>(
-    <ul className={classes.NavigationItems}>
-        <NavigationItem link="/" active>Burger Builder</NavigationItem>
-        <NavigationItem link="/">Burger Checkout</NavigationItem>
-    </ul>
+const navigationItems = () => (
+  <ul className={classes.NavigationItems}>
+    <NavigationItem link="/" exact>
+      Burger Builder
+    </NavigationItem>
+    <NavigationItem link="/orders">ORDERS</NavigationItem>
+  </ul>
 );
 
 export default navigationItems;
