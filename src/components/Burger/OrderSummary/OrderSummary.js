6bc75a27b8ce3dea98349c9e6a902dea7f517284
Tushar <tushardahiya84@gmail.com>
2020-06-25 15:35:38 +0530

    misc

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,21 +4,21 @@ import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
     // This could be a functional component , doesn't have to be a class
-    componentWillUpdate(){
+    componentWillUpdate() {
         console.log('[ordersummary] will update');
     }
 
-    render(){
-        const ingredientSummary= Object.keys(this.props.ingredients)
-        .map(igkey=>{
-            return(
-            <li key={igkey}>
-                <span style={{textTransform:'capitalize'}}>{igkey}</span>: {this.props.ingredients[igkey]} 
-            </li>)
-});  
-        return(<Auxiliary>
-            <h3>YOUR ORDER</h3>
-            <p>A delicious burger with the following ingredients :</p>
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(igkey => {
+                return (
+                    <li key={igkey}>
+                        <span style={{ textTransform: 'capitalize' }}>{igkey}</span>: {this.props.ingredients[igkey]}
+                    </li>)
+            });
+        return (<Auxiliary>
+            <h3>YOUR ORDER !</h3>
+            <p>A delicious burger with the following ingredients:</p>
             <ul>
                 {ingredientSummary}
             </ul>
