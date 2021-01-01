39cb92bbb867e51ab44e3e027864dc1e5b08af9b
tushardahiya <tushardahiya84@gmail.com>
2021-01-01 21:14:02 +0530

    combined reducers

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -30,6 +30,8 @@ const reducer = (state= initialState , action ) => {
                 ...state,
                 loading:false
             }
+        default:
+            return state;
     }
 }
 
