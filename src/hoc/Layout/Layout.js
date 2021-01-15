97ec84cfd38e243ea74f63634960ec288e9188e2
tushardahiya <tushardahiya84@gmail.com>
2021-01-15 20:06:37 +0530

    changed authenticate to logout when authencated (token present)

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -3,6 +3,7 @@ import Auxiliary from '../Auxiliary/Auxiliary';
 import classes from './Layout.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+import {connect} from 'react-redux';
 
 class Layout extends Component {
     state={
@@ -22,16 +23,26 @@ class Layout extends Component {
     render(){
         return(
             <Auxiliary>
-                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <Toolbar 
+                    isAuth={this.props.isAuthenticated}
+                    drawerToggleClicked={this.sideDrawerToggleHandler}/>
                 <SideDrawer 
-                open={this.state.showSideDrawer}
-                closed={this.sideDrawerClosedHandler}/>
+                    isAuth={this.props.isAuthenticated}
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler}
+                />
                 <main className={classes.Content} > 
-                {this.props.children}
+                    {this.props.children}
                 </main>
             </Auxiliary>
         )
     }
 }
 
-export default Layout;
+const mapStateToProps = (state) => {
+    return {
+        isAuthenticated: state.auth.token!==null
+    }
+}
+
+export default connect(mapStateToProps)(Layout);
