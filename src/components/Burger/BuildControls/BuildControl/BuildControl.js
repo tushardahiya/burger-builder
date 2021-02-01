bee755706f05da06015f1cb969c5bacb6938191f
tushardahiya <tushardahiya84@gmail.com>
2021-02-01 11:58:28 +0530

    changed color palette

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -3,12 +3,18 @@ import classes from './BuildControl.css';
 
 const buildControl = (props) => (
     <div className={classes.BuildControl}>
-        <div className={classes.Label}>{props.label}</div>
-        <button className={classes.Less} 
+        <div className={classes.Label}>
+            {props.label}
+        </div>
+        <button 
+        className={classes.Less} 
         onClick={props.removed}
-        disabled={props.disabled} >Less</button>
-        <button className={classes.More} 
-        onClick={props.added} >More</button>
+        disabled={props.disabled}>
+            Less</button>
+        <button 
+        className={classes.More} 
+        onClick={props.added} >
+            More</button>
     </div>
 );
 
